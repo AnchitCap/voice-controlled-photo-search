6643fbf9bcee3339bb7e8d00ab82450ea7424ca5
ronakf11 <rf1999@nyu.edu>
2020-12-13 07:08:52 +0530

    Update apigClient.js

--- apigClient.js
+++ apigClient.js
@@ -14,9 +14,9 @@
  */
 
 var apigClientFactory = {};
-apigClientFactory.newClient = function (config) {
-    var apigClient = { };
-    if(config === undefined) {
+apigClientFactory.newClient = function(config) {
+    var apigClient = {};
+    if (config === undefined) {
         config = {
             accessKey: '',
             secretKey: '',
@@ -27,33 +27,35 @@ apigClientFactory.newClient = function (config) {
             defaultAcceptType: 'application/json'
         };
     }
-    if(config.accessKey === undefined) {
+    if (config.accessKey === undefined) {
         config.accessKey = '';
     }
-    if(config.secretKey === undefined) {
+    if (config.secretKey === undefined) {
         config.secretKey = '';
     }
-    if(config.apiKey === undefined) {
+    if (config.apiKey === undefined) {
         config.apiKey = '';
     }
-    if(config.sessionToken === undefined) {
+    if (config.sessionToken === undefined) {
         config.sessionToken = '';
     }
-    if(config.region === undefined) {
+    if (config.region === undefined) {
         config.region = 'us-east-1';
     }
     //If defaultContentType is not defined then default to application/json
-    if(config.defaultContentType === undefined) {
+    if (config.defaultContentType === undefined) {
         config.defaultContentType = 'application/json';
     }
     //If defaultAcceptType is not defined then default to application/json
-    if(config.defaultAcceptType === undefined) {
+    if (config.defaultAcceptType === undefined) {
         config.defaultAcceptType = 'application/json';
     }
 
-    
+
     // extract endpoint and path from url
-    var invokeUrl = 'https://a3ah1nsgt5.execute-api.us-east-1.amazonaws.com/test-api-1';
+    // var invokeUrl = 'https://a3ah1nsgt5.execute-api.us-east-1.amazonaws.com/test-api-1';
+    var invokeUrl = 'https://a3ah1nsgt5.execute-api.us-east-1.amazonaws.com/v1';
+
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
@@ -80,14 +82,14 @@ apigClientFactory.newClient = function (config) {
     };
 
     var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
-    
-    
-    
-    apigClient.searchGet = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
+
+
+
+    apigClient.searchGet = function(params, body, additionalParams) {
+        if (additionalParams === undefined) { additionalParams = {}; }
+
         apiGateway.core.utils.assertParametersDefined(params, ['q'], ['body']);
-        
+
         var searchGetRequest = {
             verb: 'get'.toUpperCase(),
             path: pathComponent + uritemplate('/search').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -95,17 +97,17 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, ['q']),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(searchGetRequest, authType, additionalParams, config.apiKey);
     };
-    
-    
-    apigClient.searchOptions = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
+
+
+    apigClient.searchOptions = function(params, body, additionalParams) {
+        if (additionalParams === undefined) { additionalParams = {}; }
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var searchOptionsRequest = {
             verb: 'options'.toUpperCase(),
             path: pathComponent + uritemplate('/search').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -113,17 +115,17 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(searchOptionsRequest, authType, additionalParams, config.apiKey);
     };
-    
-    
-    apigClient.uploadPut = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
+
+
+    apigClient.uploadPut = function(params, body, additionalParams) {
+        if (additionalParams === undefined) { additionalParams = {}; }
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var uploadPutRequest = {
             verb: 'put'.toUpperCase(),
             path: pathComponent + uritemplate('/upload').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -131,17 +133,17 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(uploadPutRequest, authType, additionalParams, config.apiKey);
     };
-    
-    
-    apigClient.uploadOptions = function (params, body, additionalParams) {
-        if(additionalParams === undefined) { additionalParams = {}; }
-        
+
+
+    apigClient.uploadOptions = function(params, body, additionalParams) {
+        if (additionalParams === undefined) { additionalParams = {}; }
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var uploadOptionsRequest = {
             verb: 'options'.toUpperCase(),
             path: pathComponent + uritemplate('/upload').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -149,11 +151,11 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(uploadOptionsRequest, authType, additionalParams, config.apiKey);
     };
-    
+
 
     return apigClient;
 };
